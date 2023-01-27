50d68c40b00c81e335e3c1b06bf91890c7bfc734
Woobeen906 <siugan@naver.com>
2023-01-27 23:32:19 +0900

    ADD : 해쉬태그 기본 구성 구현 (엔터 or 스페이스 입력시 태그 추가되도록 수정 필요)

---
+++ src/components/Hashtag/Hashtag.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./Hashtag.scss";
+
+const extractHashtags = (text) => {
+  return text.match(/#\w+/g);
+};
+
+const HashtagInput = ({ hashtags, setHashtags }) => {
+  const [newTag, setNewTag] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setHashtags([...hashtags, newTag]);
+    setNewTag("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={newTag}
+        onChange={(e) => setNewTag(e.target.value)}
+        placeholder="원하는 태그 검색"
+        className="hashtag-input"
+      />
+      {/* <button type="submit">Add</button> */}
+    </form>
+  );
+};
+
+const Hashtag = ({ tag }) => {
+  return <a href={`/hashtag/${tag.substring(1)}`}>{tag}</a>;
+};
+
+const HashtagList = ({ hashtags }) => {
+  return (
+    <div>
+      {hashtags.map((tag) => (
+        <Hashtag key={tag} tag={tag} />
+      ))}
+    </div>
+  );
+};
+
+const HashtagManager = () => {
+  const [hashtags, setHashtags] = useState([]);
+  return (
+    <>
+      <HashtagInput hashtags={hashtags} setHashtags={setHashtags} />
+      <HashtagList hashtags={hashtags} />
+    </>
+  );
+};
+
+export default HashtagManager;
