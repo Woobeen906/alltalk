c8714ac5d4b2fa2ad043c95260adc080742e74a4
Woobeen906 <siugan@naver.com>
2023-01-31 22:02:15 +0900

    FIX : 헤더 포커싱 작업

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,49 @@
 import React from "react";
 import "./Header.scss";
 
+import { Link, useLocation } from "react-router-dom";
+
 const Header = () => {
+  const location = useLocation();
+
   return (
     <header className="Header">
       <div className="title">ALL TALK</div>
       <nav>
         <ul>
-          <li>콘텐츠</li>
-          <li>스토리</li>
-          <li>기업소개</li>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <li
+              style={
+                location.pathname === "/"
+                  ? { color: "#00cb8e" }
+                  : { color: "black" }
+              }
+            >
+              콘텐츠
+            </li>
+          </Link>
+          <Link to="story" style={{ textDecoration: "none" }}>
+            <li
+              style={
+                location.pathname === "/story"
+                  ? { color: "#00cb8e" }
+                  : { color: "black" }
+              }
+            >
+              스토리
+            </li>
+          </Link>
+          <Link to="" style={{ textDecoration: "none" }}>
+            <li
+              style={
+                location.pathname === "/?"
+                  ? { color: "#00cb8e" }
+                  : { color: "black" }
+              }
+            >
+              기업소개
+            </li>
+          </Link>
         </ul>
       </nav>
       <span className="login-container">
