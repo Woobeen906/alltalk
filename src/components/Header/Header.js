1fd8e481f99a6c0f1cb371f8fa2b0485e3f09286
Woobeen906 <siugan@naver.com>
2023-02-12 04:16:11 +0900

    FIX : 헤더 가로 사이즈 크기 작아질 때 글씨 줄 바뀜 현상 수정

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,55 +1,67 @@
 import React from "react";
 import "./Header.scss";
 
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
   const location = useLocation();
-
+  const navigate = useNavigate();
   return (
     <header className="Header">
-      <div className="title">ALL TALK</div>
-      <nav>
-        <ul>
-          <Link to="/" style={{ textDecoration: "none" }}>
-            <li
-              style={
-                location.pathname === "/"
-                  ? { color: "#00cb8e" }
-                  : { color: "black" }
-              }
-            >
-              콘텐츠
-            </li>
-          </Link>
-          <Link to="story" style={{ textDecoration: "none" }}>
-            <li
-              style={
-                location.pathname === "/story"
-                  ? { color: "#00cb8e" }
-                  : { color: "black" }
-              }
-            >
-              스토리
-            </li>
-          </Link>
-          <Link to="" style={{ textDecoration: "none" }}>
-            <li
-              style={
-                location.pathname === "/?"
-                  ? { color: "#00cb8e" }
-                  : { color: "black" }
-              }
-            >
-              기업소개
-            </li>
-          </Link>
-        </ul>
-      </nav>
-      <span className="login-container">
-        <button className="loginBtn">로그인</button>
-        <button className="signUpBtn">회원가입</button>
-      </span>
+      <div
+        className="title"
+        onClick={() => navigate("/")}
+        style={{ cursor: "pointer" }}
+      >
+        ALL TALK
+      </div>
+      <div className="header-left">
+        <nav>
+          <ul>
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <li
+                style={
+                  location.pathname === "/"
+                    ? { color: "#00cb8e" }
+                    : { color: "black" }
+                }
+              >
+                콘텐츠
+              </li>
+            </Link>
+            <Link to="story" style={{ textDecoration: "none" }}>
+              <li
+                style={
+                  location.pathname === "/story"
+                    ? { color: "#00cb8e" }
+                    : { color: "black" }
+                }
+              >
+                스토리
+              </li>
+            </Link>
+            <Link to="writearticle" style={{ textDecoration: "none" }}>
+              <li
+                style={
+                  location.pathname === "/writearticle"
+                    ? { color: "#00cb8e" }
+                    : { color: "black" }
+                }
+              >
+                기업소개
+              </li>
+            </Link>
+          </ul>
+        </nav>
+        <span className="login-container">
+          <button className="loginBtn" onClick={() => navigate("/SignIn")}>
+            로그인
+          </button>
+          <button className="signUpBtn" onClick={() => navigate("/SignUp")}>
+            회원가입
+          </button>
+        </span>
+      </div>
     </header>
   );
 };
