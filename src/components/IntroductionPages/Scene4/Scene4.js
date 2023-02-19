b94a0e995dcb2e137e35f7b869652c6fe505857d
Woobeen906 <siugan@naver.com>
2023-02-20 01:37:25 +0900

    ADD : 기업소개 4번 페이지 구현

---
+++ src/components/IntroductionPages/Scene4/Scene4.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Scene4.scss";
+
+import video from "assets/imgs/Scene4/play.mp4";
+
+const Scene4 = () => {
+  return (
+    <div className="scene4">
+      <div className="scene4-textbox">
+        <div className="scene4-title">
+          실시간 관리
+          <br /> 무인분리수거함
+        </div>
+        <div className="scene4-question">
+          우리는 올바른 분리수거를 하고 있음에도
+          <br /> 왜 쓰레기 문제는 계속 발생하고
+          <br />
+          분리수거함은 관리가 되지 않을까요?
+        </div>
+        <div className="scene4-answer">
+          ALL TALK의
+          <br /> 쓰레기도 쉽게 버리고 포인트도 제공하는 <br />
+          'AI기반 실시간 관리 무인 분리수거함'을 참여해보세요!
+        </div>
+      </div>
+      <div className="scene4-videobox">
+        <video src={video} controls></video>
+      </div>
+    </div>
+  );
+};
+
+export default Scene4;
