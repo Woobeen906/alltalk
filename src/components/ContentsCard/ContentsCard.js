777aa2407441f1c6e1af9a2ff056a8b6143b037f
Woobeen906 <siugan@naver.com>
2023-02-26 03:09:52 +0900

    FIX : 메인화면 콘텐츠 카드 디데이 수정

--- src/components/ContentsCard/ContentsCard.js
+++ src/components/ContentsCard/ContentsCard.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ContentsCard.scss";
 
 import Tag from "components/Tag/Tag";
+import { getDayMinuteCounter } from "assets/utils/getDayCouter";
 
 const ContentsCard = (props) => {
-  const { image, tags, title, content, member, maxMember } = props;
-  const deadline = maxMember - member === 1;
+  const { image, tags, title, content, member, maxMember, deadline } = props;
+  const memberDeadline = maxMember - member === 1;
 
   return (
     <div className="contentsCard">
@@ -13,10 +14,10 @@ const ContentsCard = (props) => {
         className="contentsCard-image-dday"
         style={image ? {} : { margin: 0 }}
       >
-        D-2
+        {`D-${getDayMinuteCounter(deadline).slice(0, 1)}`}
       </div>
-      {deadline && (
-        <div className="contentsCard-deadline-message">마감임박</div>
+      {memberDeadline && (
+        <div className="contentsCard-deadlie-message">마감임박</div>
       )}
       {image && (
         <div className="contentsCard-image">
@@ -30,23 +31,13 @@ const ContentsCard = (props) => {
         {tags && (
           <div className="contentsCard-tag">
             {tags.map((tag, index) => (
-              <Tag text={tag} id={index} />
+              <Tag text={tag} id={index} key={`${tag}index`} />
             ))}
           </div>
         )}
-        {!title && (
-          <div className="contentsCard-title">
-            {title}
-            asdakljsdfhakjsdhfakjsdhfkajsdfasdakljsdfhakjsdhfakjsdhfkajsdfasdakljsdfhakjsdhfakjsdhfkajsdfasdakljsdfhakjsdhfakjsdhfkajsdf
-          </div>
-        )}
+        {!title && <div className="contentsCard-title">{title}</div>}
 
-        {!image && (
-          <div className="contentsCard-content">
-            {content}
-            asdakljsdfhakjsdhfakjsdhfkajsdfasdakljsdfhakjsdhfakjsdhfkajsdfasdakljsdfhakjsdhfakjsdhfkajsdfasdakljsdfhakjsdhfakjsdhfkajsdfasdakljsdfhakjsdhfakjsdhfkajsdfasdakljsdfhakjsdhfakjsdhfkajsdfasdakljsdfhakjsdhfakjsdhfkajsdfasdakljsdfhakjsdhfakjsdhfkajsdfasdakljsdfhakjsdhfakjsdhfkajsdfasdakljsdfhakjsdhfakjsdhfkajsdfasdakljsdfhakjsdhfakjsdhfkajsdfasdakljsdfhakjsdhfakjsdhfkajsdf
-          </div>
-        )}
+        {!image && <div className="contentsCard-content">{content}</div>}
         <div className="contentsCard-number">
           <img
             src={require("assets/imgs/memberNumber.jpg")}
@@ -54,7 +45,7 @@ const ContentsCard = (props) => {
           />
           모집 인원{" "}
           <strong
-            style={deadline ? { color: "red" } : { color: "black" }}
+            style={memberDeadline ? { color: "red" } : { color: "black" }}
           >{`${member}/${maxMember}`}</strong>
         </div>
       </div>
