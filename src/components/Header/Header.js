7b493e64cf362c1eea38b004653d0a579bc8ce64
Woobeen906 <siugan@naver.com>
2023-03-04 01:44:29 +0900

    ADD : 모바일 디자인 추가

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import React, { useContext, useEffect, useState } from "react";
 import "./Header.scss";
 
+import { useMediaQuery } from "react-responsive";
+
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import ProfileCard from "components/ProfileCard/ProfileCard";
 
 const Header = (props) => {
   const { landing, page, onTop } = props;
+
+  const isMobile = useMediaQuery({
+    query: "(min-width:960px)",
+  });
 
   const location = useLocation();
   const navigate = useNavigate();
@@ -43,116 +49,72 @@ const Header = (props) => {
       }}
       onClick={onTopBtn}
     >
-      <div
-        className="title"
-        onClick={() => navigate("/")}
-        style={{ cursor: "pointer", color: headerCss && "#ffffff" }}
-      >
-        ALL TALK
-      </div>
-
-      <div className="header-left">
-        {pathname !== "/WriteArticle" && (
-          <nav>
-            <ul>
-              <Link to="/" style={{ textDecoration: "none" }}>
-                <li
-                  style={
-                    pathname === "/"
-                      ? { color: "#00cb8e" }
-                      : { color: headerCss ? "#ffffff" : "#191919" }
-                  }
-                >
-                  콘텐츠
-                </li>
-              </Link>
-              <Link to="/story" style={{ textDecoration: "none" }}>
-                <li
-                  style={
-                    pathname === "/story"
-                      ? { color: "#00cb8e" }
-                      : { color: headerCss ? "#ffffff" : "#191919" }
-                  }
-                >
-                  스토리
-                </li>
-              </Link>
-              {pathname === "/Introduction" ? (
-                <li
-                  onClick={onTop}
-                  style={
-                    pathname === "/Introduction"
-                      ? { color: "#00cb8e" }
-                      : { color: headerCss ? "#ffffff" : "#191919" }
-                  }
-                >
-                  기업소개
-                </li>
-              ) : (
-                <Link to="/Introduction" style={{ textDecoration: "none" }}>
-                  <li
-                    style={
-                      pathname === "/Introduction"
-                        ? { color: "#00cb8e" }
-                        : { color: headerCss ? "#ffffff" : "#191919" }
-                    }
-                  >
-                    기업소개
-                  </li>
-                </Link>
-              )}
-            </ul>
-          </nav>
-        )}
-        <span className="login-container">
-          {pathname === "/WriteArticle" ? (
-            <>
-              <button className="loadBtn">불러오기</button>
-              <button
-                className="loginBtn"
-                style={{
-                  backgroundColor: headerCss && "transparent",
-                  color: headerCss ? "#ffffff" : "#616269",
-                }}
-                onClick={() => navigate("/SignIn")}
-              >
-                임시저장
-              </button>
-              <button
-                className="signUpBtn"
-                style={{
-                  backgroundColor: headerCss && "transparent",
-                }}
-                onClick={() => navigate("/SignUp")}
-              >
-                업로드
-              </button>
-            </>
-          ) : (
-            <>
-              {localStorage.getItem("login") ? (
+      {isMobile ? (
+        <>
+          <div
+            className="title"
+            onClick={() => navigate("/")}
+            style={{ cursor: "pointer", color: headerCss && "#ffffff" }}
+          >
+            ALL TALK
+          </div>
+          <div className="header-content">
+            {pathname !== "/WriteArticle" && (
+              <nav>
+                <ul>
+                  <Link to="/" style={{ textDecoration: "none" }}>
+                    <li
+                      style={
+                        pathname === "/"
+                          ? { color: "#00cb8e" }
+                          : { color: headerCss && "#ffffff" }
+                      }
+                    >
+                      콘텐츠
+                    </li>
+                  </Link>
+                  <Link to="/story" style={{ textDecoration: "none" }}>
+                    <li
+                      style={
+                        pathname === "/story"
+                          ? { color: "#00cb8e" }
+                          : { color: headerCss && "#ffffff" }
+                      }
+                    >
+                      스토리
+                    </li>
+                  </Link>
+                  {pathname === "/Introduction" ? (
+                    <li
+                      onClick={onTop}
+                      style={
+                        pathname === "/Introduction"
+                          ? { color: "#00cb8e" }
+                          : { color: headerCss && "#ffffff" }
+                      }
+                    >
+                      기업소개
+                    </li>
+                  ) : (
+                    <Link to="/Introduction" style={{ textDecoration: "none" }}>
+                      <li
+                        style={
+                          pathname === "/Introduction"
+                            ? { color: "#00cb8e" }
+                            : { color: headerCss && "#ffffff" }
+                        }
+                      >
+                        기업소개
+                      </li>
+                    </Link>
+                  )}
+                </ul>
+              </nav>
+            )}
+            <span className="login-container">
+              {pathname === "/WriteArticle" ? (
                 <>
-                  <button
-                    className="loginBtn"
-                    style={{
-                      backgroundColor: headerCss && "transparent",
-                      color: headerCss ? "#ffffff" : "#616269",
-                    }}
-                    onClick={() => navigate("/WriteArticle")}
-                  >
-                    새 스토리 작성
-                  </button>
-                  <button
-                    className="profileBtn"
-                    style={{
-                      backgroundColor: headerCss && "transparent",
-                    }}
-                    onClick={() => handleModal()}
-                  ></button>
-                  {modal && <ProfileCard onHandleModal={handleModal} />}
-                </>
-              ) : (
-                <>
+                  <button className="loadBtn">불러오기</button>
                   <button
                     className="loginBtn"
                     style={{
@@ -161,7 +123,7 @@ const Header = (props) => {
                     }}
                     onClick={() => navigate("/SignIn")}
                   >
-                    로그인
+                    임시저장
                   </button>
                   <button
                     className="signUpBtn"
@@ -170,14 +132,203 @@ const Header = (props) => {
                     }}
                     onClick={() => navigate("/SignUp")}
                   >
-                    회원가입
+                    업로드
                   </button>
                 </>
+              ) : (
+                <>
+                  {localStorage.getItem("login") ? (
+                    <>
+                      <button
+                        className="loginBtn"
+                        style={{
+                          backgroundColor: headerCss && "transparent",
+                          color: headerCss ? "#ffffff" : "#616269",
+                        }}
+                        onClick={() => navigate("/WriteArticle")}
+                      >
+                        새 스토리 작성
+                      </button>
+                      <button
+                        className="profileBtn"
+                        style={{
+                          backgroundColor: headerCss && "transparent",
+                        }}
+                        onClick={() => handleModal()}
+                      ></button>
+                      {modal && <ProfileCard onHandleModal={handleModal} />}
+                    </>
+                  ) : (
+                    <>
+                      <button
+                        className="loginBtn"
+                        style={{
+                          backgroundColor: headerCss && "transparent",
+                          color: headerCss ? "#ffffff" : "#616269",
+                        }}
+                        onClick={() => navigate("/SignIn")}
+                      >
+                        로그인
+                      </button>
+                      <button
+                        className="signUpBtn"
+                        style={{
+                          backgroundColor: headerCss && "transparent",
+                        }}
+                        onClick={() => navigate("/SignUp")}
+                      >
+                        회원가입
+                      </button>
+                    </>
+                  )}
+                </>
               )}
-            </>
-          )}
-        </span>
-      </div>
+            </span>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="header-mobile-top">
+            <div
+              className="title"
+              onClick={() => navigate("/")}
+              style={{ cursor: "pointer", color: headerCss && "#ffffff" }}
+            >
+              ALL TALK
+            </div>
+            <div className="header-content">
+              <span className="login-container">
+                {pathname === "/WriteArticle" ? (
+                  <>
+                    <button className="loadBtn">불러오기</button>
+                    <button
+                      className="loginBtn"
+                      style={{
+                        backgroundColor: headerCss && "transparent",
+                        color: headerCss ? "#ffffff" : "#616269",
+                      }}
+                      onClick={() => navigate("/SignIn")}
+                    >
+                      임시저장
+                    </button>
+                    <button
+                      className="signUpBtn"
+                      style={{
+                        backgroundColor: headerCss && "transparent",
+                      }}
+                      onClick={() => navigate("/SignUp")}
+                    >
+                      업로드
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    {localStorage.getItem("login") ? (
+                      <>
+                        <button
+                          className="loginBtn"
+                          style={{
+                            backgroundColor: headerCss && "transparent",
+                            color: headerCss ? "#ffffff" : "#616269",
+                          }}
+                          onClick={() => navigate("/WriteArticle")}
+                        >
+                          새 스토리 작성
+                        </button>
+                        <button
+                          className="profileBtn"
+                          style={{
+                            backgroundColor: headerCss && "transparent",
+                          }}
+                          onClick={() => handleModal()}
+                        ></button>
+                        {modal && <ProfileCard onHandleModal={handleModal} />}
+                      </>
+                    ) : (
+                      <>
+                        <button
+                          className="loginBtn"
+                          style={{
+                            backgroundColor: headerCss && "transparent",
+                            color: headerCss ? "#ffffff" : "#616269",
+                          }}
+                          onClick={() => navigate("/SignIn")}
+                        >
+                          로그인
+                        </button>
+                        <button
+                          className="signUpBtn"
+                          style={{
+                            backgroundColor: headerCss && "transparent",
+                          }}
+                          onClick={() => navigate("/SignUp")}
+                        >
+                          회원가입
+                        </button>
+                      </>
+                    )}
+                  </>
+                )}
+              </span>
+            </div>
+          </div>
+
+          <div className="header-mobile-bottom">
+            {pathname !== "/WriteArticle" && (
+              <nav>
+                <ul>
+                  <Link to="/" style={{ textDecoration: "none" }}>
+                    <li
+                      style={
+                        pathname === "/"
+                          ? { color: "#00cb8e" }
+                          : { color: headerCss && "#ffffff" }
+                      }
+                    >
+                      콘텐츠
+                    </li>
+                  </Link>
+                  <Link to="/story" style={{ textDecoration: "none" }}>
+                    <li
+                      style={
+                        pathname === "/story"
+                          ? { color: "#00cb8e" }
+                          : { color: headerCss && "#ffffff" }
+                      }
+                    >
+                      스토리
+                    </li>
+                  </Link>
+                  {pathname === "/Introduction" ? (
+                    <li
+                      onClick={onTop}
+                      style={
+                        pathname === "/Introduction"
+                          ? { color: "#00cb8e" }
+                          : { color: headerCss && "#ffffff" }
+                      }
+                    >
+                      기업소개
+                    </li>
+                  ) : (
+                    <Link to="/Introduction" style={{ textDecoration: "none" }}>
+                      <li
+                        style={
+                          pathname === "/Introduction"
+                            ? { color: "#00cb8e" }
+                            : { color: headerCss && "#ffffff" }
+                        }
+                      >
+                        기업소개
+                      </li>
+                    </Link>
+                  )}
+                </ul>
+              </nav>
+            )}
+          </div>
+        </>
+      )}
     </header>
   );
 };
